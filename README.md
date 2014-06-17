1406430cafa98671c9a5023c244bb7724c2da5b1
a5mith <aaronsmith91@sky.com>
2014-06-17 20:05:39 +0100

    0.0.1
    Should be good.

---
+++ README.md
@@ -0,0 +1,13 @@
+# NodeBB jsFiddle Plugin
+
+This NodeBB plugin allows users to embed jsFiddle pages inline in their posts.
+
+## Installation
+
+    npm install nodebb-plugin-jsfiddle
+
+## Usage
+
+    Paste the URL into your post.
+    ???
+    Profit!
---
+++ library.js
@@ -0,0 +1,14 @@
+(function(module) {
+    "use strict";
+
+    var jsFiddle = {},
+        embed = '<iframe width="100%" height="300" src="http://jsfiddle.net/$1/embedded/result,js,html,css/" allowfullscreen="allowfullscreen" frameborder="0"></iframe>';
+
+
+    jsFiddle.parse = function(postContent, callback) {
+        postContent = postContent.replace(/<a href="(?:http?:\/\/)?(?:jsfiddle\.net)\/?(.+)">.+<\/a>/g, embed);
+        callback(null, postContent);
+    };
+
+    module.exports = jsFiddle;
+}(module));
