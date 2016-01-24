3d5afa7c1751dd83ec9fd1f4f207f40e4e19e601
Agus Syahputra <agus.putra99@gmail.com>
2016-01-24 13:32:53 +0700

    Update library.js
    Update parse function and add support for jsBin

--- library.js
+++ library.js
@@ -1,14 +1,17 @@
-(function(module) {
-    "use strict";
+(function (module) {
+	"use strict";
 
-    var jsFiddle = {},
-        embed = '<iframe width="100%" height="300" src="http://jsfiddle.net/$1embedded/result,js,html,css/" allowfullscreen="allowfullscreen" frameborder="0"></iframe>';
+	var jsFiddle = {},
+		embed = '<iframe width="100%" height="300" src="http://jsfiddle.net/$2/embedded/result,js,html,css/" allowfullscreen="allowfullscreen" frameborder="0"></iframe>',
+		embedJsBin = '<iframe width="100%" height="300" src="http://jsbin.com/$2" allowfullscreen="allowfullscreen" frameborder="0"></iframe>'
 
 
-    jsFiddle.parse = function(postContent, callback) {
-        postContent = postContent.replace(/<a href="(?:http?:\/\/)?(?:jsfiddle\.net)\/?(.+)">.+<\/a>/g, embed);
-        callback(null, postContent);
-    };
+	jsFiddle.parse = function (postContent, callback) {
+		postContent.postData.content = postContent.postData.content
+			.replace(/<a(.?|.+)href=["|']http[s]?:\/\/jsfiddle\.net\/(\w+)\/?["|'](.?|.+)>.+<\/a>/g, embed)
+			.replace(/<a(.?|.+)href=["|']http[s]?:\/\/jsbin\.com\/(\w+\/edit\?[\w|,]+)\/?["|'](.?|.+)>.+<\/a>/g, embedJsBin);
+		callback(null, postContent);
+	};
 
-    module.exports = jsFiddle;
+	module.exports = jsFiddle;
 }(module));
