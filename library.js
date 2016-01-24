f13678b3963b0d50ba504ab8a25078864210a154
Agus Syahputra <agus.putra99@gmail.com>
2016-01-24 16:01:46 +0700

    Update library.js

--- library.js
+++ library.js
@@ -2,16 +2,17 @@
 	"use strict";
 
 	var jsFiddle = {},
-		embed = '<iframe width="100%" height="300" src="http://jsfiddle.net/$1" allowfullscreen="allowfullscreen" frameborder="0"></iframe>',
+		embed = '<iframe width="100%" height="300" src="http://jsfiddle.net/$1/embedded/result,js,html,css" allowfullscreen="allowfullscreen" frameborder="0"></iframe>',
 		embedJsBin = '<iframe width="100%" height="300" src="http://jsbin.com/$1" allowfullscreen="allowfullscreen" frameborder="0"></iframe>'
 
 
 	jsFiddle.parse = function (postContent, callback) {
 		postContent.postData.content = postContent.postData.content
-			.replace(/<a.+?href=["|']http[s]?:\/\/jsfiddle\.net\/([\w|\/|\,|?]+)+["|'].+?>.+<\/a>/g, embed)
+			//We just want the first one or two segments after host name
+			.replace(/<a.+?href=["|']http[s]?:\/\/jsfiddle\.net((\/\w+){0,2}).+?["|'].+?>.+<\/a>/g, embed)
+			//We just the first one or more segments after host name
 			.replace(/<a.+?href=["|']http[s]?:\/\/jsbin\.com\/([\w|\/|\,|?]+)+["|'].+?>.+<\/a>/g, embedJsBin);
 		callback(null, postContent);
-		console.log(postContent.postData.content)
 	};
 
 	module.exports = jsFiddle;
