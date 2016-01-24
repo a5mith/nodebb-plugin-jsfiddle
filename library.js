32f73eaba7367212380a411b6146f72d7202b820
Agus Syahputra <agus.putra99@gmail.com>
2016-01-24 15:11:44 +0700

    Update library.js

--- library.js
+++ library.js
@@ -2,15 +2,16 @@
 	"use strict";
 
 	var jsFiddle = {},
-		embed = '<iframe width="100%" height="300" src="http://jsfiddle.net/$2/embedded/result,js,html,css/" allowfullscreen="allowfullscreen" frameborder="0"></iframe>',
-		embedJsBin = '<iframe width="100%" height="300" src="http://jsbin.com/$2" allowfullscreen="allowfullscreen" frameborder="0"></iframe>'
+		embed = '<iframe width="100%" height="300" src="http://jsfiddle.net/$1" allowfullscreen="allowfullscreen" frameborder="0"></iframe>',
+		embedJsBin = '<iframe width="100%" height="300" src="http://jsbin.com/$1" allowfullscreen="allowfullscreen" frameborder="0"></iframe>'
 
 
 	jsFiddle.parse = function (postContent, callback) {
 		postContent.postData.content = postContent.postData.content
-			.replace(/<a(.?|.+)href=["|']http[s]?:\/\/jsfiddle\.net\/(\w+)\/?["|'](.?|.+)>.+<\/a>/g, embed)
-			.replace(/<a(.?|.+)href=["|']http[s]?:\/\/jsbin\.com\/(\w+\/edit\?[\w|,]+)\/?["|'](.?|.+)>.+<\/a>/g, embedJsBin);
+			.replace(/<a.+?href=["|']http[s]?:\/\/jsfiddle\.net\/([\w|\/|\,|?]+)+["|'].+?>.+<\/a>/g, embed)
+			.replace(/<a.+?href=["|']http[s]?:\/\/jsbin\.com\/([\w|\/|\,|?]+)+["|'].+?>.+<\/a>/g, embedJsBin);
 		callback(null, postContent);
+		console.log(postContent.postData.content)
 	};
 
 	module.exports = jsFiddle;
