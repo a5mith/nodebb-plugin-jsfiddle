aa0911f4225b807b51f59d439655c0aaed1c51a6
Agus Syahputra <agus.putra99@gmail.com>
2016-01-30 07:33:42 +0700

    Update library.js

--- library.js
+++ library.js
@@ -10,7 +10,7 @@
 		postContent.postData.content = postContent.postData.content
 			//We just want the first one or two segments after host name
 			.replace(/<a.+?href=["|']http[s]?:\/\/jsfiddle\.net((\/\w+){0,2}).+?["|'].+?>.+<\/a>/g, embed)
-			//We just the first one or more segments after host name
+			//We just want the first one or more segments after host name
 			.replace(/<a.+?href=["|']http[s]?:\/\/jsbin\.com\/([\w|\/|\,|?]+)+["|'].+?>.+<\/a>/g, embedJsBin);
 		callback(null, postContent);
 	};
